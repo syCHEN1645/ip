50d9a163ed5211a1faa89175d77f50b76373d2ba
syCHEN1645 <e1121133@u.nus.edu>
2025-01-21 19:43:45 +0800

    Finish level-0

---
+++ src/main/java/Rook.java
@@ -0,0 +1,34 @@
+public class Rook {
+    public static void main(String[] args) {
+        String botName = "Rook";
+        String logo = """
+                \t___ ___ ___
+                \t| |_| |_| |
+                \t \\       /
+                \t  |     |
+                \t  |     |
+                \t  |_____|
+                \t /-------\\
+                \t|_________|""";
+        String partition = "------------------------------------------";
+        System.out.println(partition);
+        System.out.println("Hello, I am " + botName + "!");
+        System.out.println(logo);
+        System.out.println("What can I do for you?");
+        System.out.println(partition);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(partition);
+    }
+}
+
+/*
+___ ___ ___
+| |_| |_| |
+ \       /
+  |     |
+  |     |
+  |_____|
+ /-------\
+|_________|
+
+ */
