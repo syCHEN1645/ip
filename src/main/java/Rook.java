990d9a33b2871e782dbb1f1990bf81919164e465
syCHEN1645 <e1121133@u.nus.edu>
2025-01-25 15:40:25 +0800

    Finish level-3
    Amend some naming errors
    Improve code structure by moving while loop from methods to main
    Add new methods to check if a message is a proper command
    Add new methods for Rook to mark & unmark activities

--- src/main/java/Rook.java
+++ src/main/java/Rook.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Rook {
@@ -19,67 +16,155 @@ public class Rook {
     static String partition = "------------------------------------------";
     static String exitCommand = "bye";
     static String listCommand = "list";
-    static List<String> activities = new ArrayList<>();
+    static String unmarkCommand = "unmark";
+    static String markCommand = "mark";
+    static List<Activity> activities = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println(partition);
-        ChatGreeting();
-        // ChatEcho();
-        ChatAddList();
+        chatGreeting();
+        while (true) {
+            String message = scanner.nextLine();
+
+            if (message.equals(exitCommand)) {
+                chatBye();
+                break;
+            } else if (message.equals(listCommand)) {
+                chatShowList();
+            } else if (isUnmarkCommand(message)) {
+                int index = convertStringToInt(message.split(" ")[1]);
+                chatUnmarkDone(index);
+            } else if (isMarkCommand(message)) {
+                int index = convertStringToInt(message.split(" ")[1]);
+                chatMarkDone(index);
+            } else {
+                chatAddList(message);
+            }
+        }
     }
 
-    static void ChatGreeting() {
-        System.out.println("Greetings, my Lord, I am " + botName + "!");
-        System.out.println(logo);
-        System.out.println("How may I serve you?");
+    private static boolean isMarkCommand(String message) {
+        String[] words = message.split(" ");
+        return words.length == 2 && words[0].equals(markCommand)
+                && convertStringToInt(words[1]) > 0
+                && convertStringToInt(words[1]) <= activities.size();
+    }
+
+    private static boolean isUnmarkCommand(String message) {
+        String[] words = message.split(" ");
+        return words.length == 2 && words[0].equals(unmarkCommand)
+                && convertStringToInt(words[1]) > 0
+                && convertStringToInt(words[1]) <= activities.size();
+    }
+
+    private static int convertStringToInt(String str) {
+        int result = 0;
+        char[] chars = str.toCharArray();
+        int[] digits = new int[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            digits[i] = Character.digit(chars[i], 10);
+            if (digits[i] < 0) {
+                // if this char is not a number, then return -1
+                return -1;
+            }
+            result *= 10;
+            result += digits[i];
+        }
+        return result;
+    }
+
+    private static void chatMarkDone(int index) {
+        activities.get(index - 1).setDone(true);
+        System.out.println(partition);
+        System.out.println(activities.get(index - 1));
+        System.out.println("My lord, Activity " + index + " has been marked under your command!");
         System.out.println(partition);
     }
 
-    static void ChatEcho() {
-        String exitCommand = "bye";
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            String command = scanner.nextLine();
-            if (command.equals(exitCommand)) {
-                System.out.println(partition);
-                System.out.println("Farewell, my Lord!");
-                System.out.println(partition);
-                break;
-            } else {
-                System.out.println(partition);
-                System.out.println(command);
-                System.out.println(partition);
-            }
-        }
+    private static void chatUnmarkDone(int index) {
+        activities.get(index - 1).setDone(false);
+        System.out.println(partition);
+        System.out.println(activities.get(index - 1));
+        System.out.println("My lord, Activity " + index + " has been unmarked under your command!");
+        System.out.println(partition);
     }
 
-    static void ChatAddList() {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            String message = scanner.nextLine();
-            if (message.equals(exitCommand)) {
-                System.out.println(partition);
-                System.out.println("Farewell, my Lord!");
-                System.out.println(partition);
-                break;
-            } else if (message.equals(listCommand)) {
-                System.out.println(partition);
-                if (activities.isEmpty()) {
-                    System.out.println("You have no activities, my Lord.");
-                } else {
-                    System.out.println("Here are your activities, my Lord.");
-                    for (int i = 0; i < activities.size(); i++) {
-                        System.out.println(i + 1 + ". " + activities.get(i));
-                    }
-                }
-                System.out.println(partition);
-            } else {
-                System.out.println(partition);
-                activities.add(message);
-                System.out.println(message + " is added.");
-                System.out.println(partition);
+    private static void chatAddList(String message) {
+        System.out.println(partition);
+        activities.add(new Activity(message));
+        System.out.println(message + " is added.");
+        System.out.println(partition);
+    }
+
+    private static void chatShowList() {
+        System.out.println(partition);
+        if (activities.isEmpty()) {
+            System.out.println("You have no activities, my Lord.");
+        } else {
+            System.out.println("Here are your activities, my Lord.");
+            for (int i = 0; i < activities.size(); i++) {
+                System.out.println(i + 1 + ". " + activities.get(i));
             }
         }
+        System.out.println(partition);
+    }
+
+    private static void chatGreeting() {
+        System.out.println("Greetings, my Lord, " + botName + " at your service!");
+        System.out.println(logo);
+        System.out.println("How may I assist you?");
+        System.out.println(partition);
+    }
+
+//    private static void chatEcho() {
+//        while (true) {
+//            String message = scanner.nextLine();
+//            if (message.equals(exitCommand)) {
+//                System.out.println(partition);
+//                System.out.println("Farewell, my Lord!");
+//                System.out.println(partition);
+//                break;
+//            } else {
+//                System.out.println(partition);
+//                System.out.println(message);
+//                System.out.println(partition);
+//            }
+//        }
+//    }
+
+//    private static void chatAddList() {
+//        while (true) {
+//            String message = scanner.nextLine();
+//            if (message.equals(exitCommand)) {
+//                System.out.println(partition);
+//                System.out.println("Farewell, my Lord!");
+//                System.out.println(partition);
+//                break;
+//            } else if (message.equals(listCommand)) {
+//                System.out.println(partition);
+//                if (activities.isEmpty()) {
+//                    System.out.println("You have no activities, my Lord.");
+//                } else {
+//                    System.out.println("Here are your activities, my Lord.");
+//                    for (int i = 0; i < activities.size(); i++) {
+//                        System.out.println(i + 1 + ". " + activities.get(i));
+//                    }
+//                }
+//                System.out.println(partition);
+//            } else {
+//                System.out.println(partition);
+//                activities.add(new Activity(message));
+//                System.out.println(message + " is added.");
+//                System.out.println(partition);
+//            }
+//        }
+//    }
+
+    private static void chatBye() {
+        System.out.println(partition);
+        System.out.println("Farewell, my Lord!");
+        System.out.println(partition);
     }
 
 }
